df8195693c13259626a74350fff88033f881e451
Sonadi Kannangara <ksonadi@gmail.com>
2021-11-23 19:47:29 -0500

    Singleton example

---
+++ src/ca/durhamcollege/RandomVector.java
@@ -0,0 +1,44 @@
+package ca.durhamcollege;
+
+import java.util.Random;
+
+// singleton
+public class RandomVector
+{
+    private Random m_rand;
+    // Step 1. private static member
+    private static RandomVector m_instance = null;
+
+    // Step 2. make the default constructor private
+    private RandomVector()
+    {
+        m_rand = new Random();
+    }
+
+    // Step 3. make a public instance method that acts as "portal" to the class
+    public static RandomVector Instance()
+    {
+        if(m_instance == null)
+        {
+            m_instance = new RandomVector();
+        }
+        return m_instance;
+    }
+
+    public final Vector2D createVector2D(final Vector2D start, final Vector2D end)
+    {
+        //Random rand = new Random(); // instance of the random class
+
+        // generate random X values
+        float minX = Math.min(start.getX(), end.getX());
+        float maxX = Math.max(start.getX(), end.getX());
+        float randomX = (m_rand.nextFloat() * (maxX - minX)) + minX; // grab the next float and store it inside
+
+        // generate random Y values
+        float minY = Math.min(start.getY(), end.getY());
+        float maxY = Math.max(start.getY(), end.getY());
+        float randomY = (m_rand.nextFloat() * (maxY - minY)) + minY;
+
+        return new Vector2D(randomX, randomY);
+    }
+}
